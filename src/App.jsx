817fdbbb5533bb89ff8a76b3bbb9b92e5fa02d2e
Joblanco88 <jo_blanco9@yahoo.com.br>
2023-07-17 15:28:37 -0300

    Criado Header e concluido propriedades css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import Header from './components/Header';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Projects />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,16 @@ import '../styles/Header.css';
 import perfil from '../assets/images/perfil.jpeg';
 
 function Header() {
+  const descText = 'Crio ambientes de acordo com clientes, combinando funcionalidade com elegância. Objetivo transformar o espaço de acordo com a personalidade e o estilo de vida de cada um.';
   return (
     <div className='header-main'>
       <div id='firstsegment'>
         <h2 id='firstsegtext' className='header-name'>Claudia Ribeiro Lobo</h2>
-        <h3 id='firstsegtext' className='header-description'>Desenhista Projetista</h3>
+        <h3 id='firstsegtext' className='header-work'>Desenhista Projetista</h3>
       </div>
       <div id='secondsegment'>
         <img className='header-logo' src={ perfil } />
-        <div>
-          <span>Descrição</span>
-        </div>
+        <span className='header-description'>{ descText }</span>
       </div>
     </div>
   );
